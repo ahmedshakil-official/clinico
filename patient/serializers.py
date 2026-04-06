f543067973171e1f1786251354e07ccc1c34c3d0
Shakil Ahmed <ahmedshakil.official@gmail.com>
2026-04-07 02:43:30 +1000

    fixed bug

--- patient/serializers.py
+++ patient/serializers.py
@@ -8,23 +8,23 @@ User = get_user_model()
 
 
 class PatientListCreateSerializer(serializers.ModelSerializer):
-    # User fields
     alias = serializers.UUIDField(read_only=True)
     slug = serializers.CharField(read_only=True)
-    email = serializers.EmailField()
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    phone = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+
+    email = serializers.EmailField(source="user.email")
+    first_name = serializers.CharField(source="user.first_name")
+    last_name = serializers.CharField(source="user.last_name")
+    phone = serializers.CharField(source="user.phone", required=False, allow_blank=True, allow_null=True)
     title = serializers.ChoiceField(
+        source="user.title",
         choices=NameTitleChoices.choices,
         required=False,
         default=NameTitleChoices.MR,
     )
-    suburb = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    postal_code = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    address = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    profile_image = serializers.ImageField(required=False, allow_null=True)
-
+    suburb = serializers.CharField(source="user.suburb", required=False, allow_blank=True, allow_null=True)
+    postal_code = serializers.CharField(source="user.postal_code", required=False, allow_blank=True, allow_null=True)
+    address = serializers.CharField(source="user.address", required=False, allow_blank=True, allow_null=True)
+    profile_image = serializers.ImageField(source="user.profile_image", required=False, allow_null=True)
 
     class Meta:
         model = Patient
@@ -48,21 +48,29 @@ class PatientListCreateSerializer(serializers.ModelSerializer):
             "medical_history",
         ]
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("A user with this email already exists.")
-        return value
+    def validate(self, attrs):
+        user_data = attrs.get("user", {})
+        email = user_data.get("email")
+
+        if email and User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({
+                "email": "A user with this email already exists."
+            })
+
+        return attrs
 
     def create(self, validated_data):
-        email = validated_data.pop("email")
-        first_name = validated_data.pop("first_name")
-        last_name = validated_data.pop("last_name")
-        phone = validated_data.pop("phone", None)
-        title = validated_data.pop("title", NameTitleChoices.MR)
-        suburb = validated_data.pop("suburb", None)
-        postal_code = validated_data.pop("postal_code", None)
-        address = validated_data.pop("address", None)
-        profile_image = validated_data.pop("profile_image", None)
+        user_data = validated_data.pop("user", {})
+
+        email = user_data.get("email")
+        first_name = user_data.get("first_name")
+        last_name = user_data.get("last_name")
+        phone = user_data.get("phone", None)
+        title = user_data.get("title", NameTitleChoices.MR)
+        suburb = user_data.get("suburb", None)
+        postal_code = user_data.get("postal_code", None)
+        address = user_data.get("address", None)
+        profile_image = user_data.get("profile_image", None)
 
         request = self.context.get("request")
 
@@ -95,19 +103,19 @@ class PatientRetrieveUpdateSerializer(serializers.ModelSerializer):
     alias = serializers.UUIDField(read_only=True)
     slug = serializers.CharField(read_only=True)
 
-    email = serializers.EmailField(required=False)
-    first_name = serializers.CharField(required=False)
-    last_name = serializers.CharField(required=False)
-    phone = serializers.CharField( required=False, allow_blank=True, allow_null=True)
+    email = serializers.EmailField(source="user.email", required=False)
+    first_name = serializers.CharField(source="user.first_name", required=False)
+    last_name = serializers.CharField(source="user.last_name", required=False)
+    phone = serializers.CharField(source="user.phone", required=False, allow_blank=True, allow_null=True)
     title = serializers.ChoiceField(
         source="user.title",
         choices=NameTitleChoices.choices,
         required=False,
     )
-    suburb = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    postal_code = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    address = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    profile_image = serializers.ImageField(required=False, allow_null=True)
+    suburb = serializers.CharField(source="user.suburb", required=False, allow_blank=True, allow_null=True)
+    postal_code = serializers.CharField(source="user.postal_code", required=False, allow_blank=True, allow_null=True)
+    address = serializers.CharField(source="user.address", required=False, allow_blank=True, allow_null=True)
+    profile_image = serializers.ImageField(source="user.profile_image", required=False, allow_null=True)
 
     class Meta:
         model = Patient
@@ -131,11 +139,18 @@ class PatientRetrieveUpdateSerializer(serializers.ModelSerializer):
             "medical_history",
         ]
 
-    def validate_email(self, value):
-        user = self.instance.user
-        if User.objects.exclude(id=user.id).filter(email=value).exists():
-            raise serializers.ValidationError("A user with this email already exists.")
-        return value
+    def validate(self, attrs):
+        user_data = attrs.get("user", {})
+        email = user_data.get("email")
+
+        if email:
+            current_user = self.instance.user
+            if User.objects.exclude(id=current_user.id).filter(email=email).exists():
+                raise serializers.ValidationError({
+                    "email": "A user with this email already exists."
+                })
+
+        return attrs
 
     def update(self, instance, validated_data):
         user_data = validated_data.pop("user", {})
